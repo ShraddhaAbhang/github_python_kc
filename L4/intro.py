3be394fd6f52a294a26d8ba2e1875c6f4af24539
ShraddhaAbhang <shraddhasampatabhang@gmail.com>
2024-07-23 23:51:41 +0530

    Agenda of L4 and understanding range function

---
+++ L4/intro.py
@@ -0,0 +1,35 @@
+
+'''
+Agenda:
+    - for loop
+    - strings
+
+Intro:                      GVR, 1991, Free
+Features:                   Simple, Easy, PPO, OOP, Dynamically Type, I, Platform Independent and HL
+Fundamentals:               #, lowercase + None + True + False. Id, DataTypes: int, float, str and bool
+I/O:                        input(), int(), float(), str(), bool(), print()
+Operators:                  arithmetic, assignment, relational and logical op
+Selection Statement:        if,        if else,     if elif,        --> one time
+Iteration Statement:        while      for                          --> multiple time
+
+        for loop: before studing we should learn about range()
+
+        range():
+                - it is build in funtion (bif) for generating sequence of integers
+                - 3 variation:
+                            1) range(stop)
+                            2) range(start, stop)
+                            3) range(start, stop, step)
+
+                    - eg:   range(5):           0, 1, 2, 3, 4
+                            range(7):           0, 1, 2, 3, 4, 5, 6
+                            
+                            range(2, 5):        2, 3, 4
+                            range(4, 8):        4, 5, 6, 7
+                            range(5, 2):        no o/p
+
+                            range(2, 10, 2):    2, 4, 6, 8
+                            range(3, 15, 2):    3, 5, 7, 9, 11, 13
+                            range(5, 2, -1):    5, 4, 3
+                            range(10, 3, -2):   10, 8, 6, 4
+'''
